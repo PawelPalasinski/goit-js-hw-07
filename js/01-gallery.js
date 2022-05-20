96eab2e465fb02870bf4c8c218585b5f1713866d
PawelPalasinski <pawel.palasinski@gmail.com>
2022-05-21 00:27:02 +0200

    add to list

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML(
+  "beforeend",
+  galleryItems
+    .map(
+      (galleryItem) =>
+        `<div class="gallery__image"><img src="${galleryItem.preview}" alt= "${galleryItem.alt}" width="340"></div>`
+    )
+    .join("")
+);
+
+for (let i = 0; i < galleryItems.length+1; i++) {
+  let item = document.querySelectorAll("div");
+  item[i].style.margin = "10px";
+};
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// ADD ALL IMAGES
+
+const gallery = document.querySelector(".gallery");
+
+gallery.insertAdjacentHTML(
+  "beforeend",
+  galleryItems
+    .map(
+      (galleryItem) =>
+        `<li class="item"><img src="${galleryItem.preview}" alt= "${galleryItem.alt}" width="340"></li>`
+    )
+    .join("")
+);
+
+// add class for css
+
+gallery.classList.add("gallery__item");
+
+// loop to add all from gallery-items.js
+
+for (let i = 0; i < galleryItems.length; i++) {
+  let item = document.querySelectorAll("li");
+  item[i].style.margin = "10px";
+};
